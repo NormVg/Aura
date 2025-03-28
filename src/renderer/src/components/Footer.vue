8e9a65adda65d5e46620e8187b3d5b40fed33f48
Vishnu Gupta <thenormvg@gmail.com>
2025-03-29 01:51:51 +0530

    -a
    bhai mene footer bana deya ha thoda

--- src/renderer/src/components/Footer.vue
+++ src/renderer/src/components/Footer.vue
@@ -1,26 +1,24 @@
 <style scoped>
-#footer-box{
-
+#footer-box {
   position: fixed;
   bottom: 0;
   width: 100%;
-
 }
-#footer-box{
+#footer-box {
   background-color: var(--sbgc);
   height: 13px;
 
   position: fixed;
   bottom: 0;
   width: 100%;
-
 }
 
-
-#footer-text-box{
+#footer-text-box {
   background-color: var(--sbgc);
-
-  height: 50px;
+  border-top-left-radius: var(--bdr);
+  border-top-right-radius: var(--bdr);
+  /* border-radius: 100px; */
+  height: 40px;
   /* width: 100%; */
   position: fixed;
   bottom: 13px;
@@ -28,32 +26,52 @@
   transform: translateX(-50%);
 }
 
-#tb-text{
+#tb-text {
   background-color: var(--sbgc);
-  border: 1px green solid;
-  height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  height: 40px;
   width: max-content;
+  border-top-left-radius: var(--bdr);
+  border-top-right-radius: var(--bdr);
   /* position: fixed; */
   /* bottom: 13px; */
   /* left: 50%; */
   /* transform: translateX(-50%); */
 }
 
-#before-tb{
-  /* background-color: red; */
-
+#before-tb {
   background-color: transparent;
+  position: absolute;
+  bottom: 0%;
+  left: -20px;
+  /* border: 1px solid green; */
+  height: 20px;
+  width: 20px;
+}
 
+#before-tb img {
+  height: 20px;
+  width: 20px;
+}
+
+
+#after-tb {
+  background-color: transparent;
+  position: absolute;
+  bottom: 0%;
+  right: -20px;
+  /* border: 1px solid green; */
+  height: 20px;
+  width: 20px;
+}
+
+#after-tb img {
   height: 25px;
   width: 25px;
-  position: absolute;
-
-  bottom: 10px;
-
-  left: -35px;
-  border-bottom-right-radius: 10px;
-
-  box-shadow: 10px 10px 0 0 #F66969;
 }
 
 /* ::before {
@@ -67,23 +85,22 @@
   border-top-left-radius: 25px;
   box-shadow: 0 -25px 0 0 #F66969;
 } */
-
 </style>
+
+<script setup>
+import LeftCurve from "../assets/img/left-curve.svg";
+import RightCurve from "../assets/img/right-curve.svg";
+</script>
 
 <template>
   <div id="footer-box">
-
     <div id="footer-text-box">
-
-      <div id="before-tb"></div>
-      <div id="tb-text">Hello Boss</div>
-      <div id="after-tb"></div>
-
+      <div id="before-tb"><img :src="LeftCurve" /></div>
+      <div id="tb-text">Hello Boss, how are you </div>
+      <div id="after-tb"><img :src="RightCurve" /></div>
     </div>
 
-    <div id="footer-bar">
-    </div>
+    <div id="footer-bar"></div>
   </div>
 </template>
-<script setup>
-</script>
+<script setup></script>
